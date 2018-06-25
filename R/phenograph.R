9faf0868a127b568bd89074cb20454079984de34
TomKellyGenetics <tomkellygenetics@gmail.com>
2018-06-25 15:01:06 +0900

    update documentation

--- R/phenograph.R
+++ R/phenograph.R
@@ -31,14 +31,14 @@
 #'  
 #' @references Jacob H. Levine and et.al. Data-Driven Phenotypic Dissection of AML Reveals Progenitor-like Cells that Correlate with Prognosis. Cell, 2015.                
 #' @examples
-#' library("igraph")
+#' library(igraph)
 #' iris_unique <- unique(iris) # Remove duplicates
 #' data <- as.matrix(iris_unique[,1:4])
 #' Rphenograph_out <- Rphenograph(data, k = 45)
 #' modularity(Rphenograph_out[[2]])
 #' membership(Rphenograph_out[[2]])
 #' iris_unique$phenograph_cluster <- factor(membership(Rphenograph_out[[2]]))
-#' library("ggplot2")
+#' library(ggplot2)
 #' ggplot(iris_unique, aes(x=Sepal.Length, y=Sepal.Width, col=Species, shape=phenograph_cluster))
 #'     + geom_point(size = 3)+theme_bw()
 #' 
